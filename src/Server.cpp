cafa152d0db7bca9b269e7238f01486a353605a8
i99dev <obaid.altuniji@gmail.com>
2022-11-21 17:35:42 +0400

    update code

--- src/Server.cpp
+++ src/Server.cpp
@@ -6,151 +6,167 @@
 /*   By: oal-tena <oal-tena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 11:10:58 by oal-tena          #+#    #+#             */
-/*   Updated: 2022/11/16 06:09:11 by oal-tena         ###   ########.fr       */
+/*   Updated: 2022/11/21 17:24:31 by oal-tena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incl/Server.hpp"
+#include <netdb.h>
 
-ft::Server::Server(std::string const &port, std::string const &password):
-    host("localhost"),
-    servername("ft_irc"),
-    version("0.1"),
-    port(port),
-    password(password)
+ft::Server::Server(std::string const &port, std::string const &password) : host("127.0.0.1"),
+                                                                           servername("ft_irc"),
+                                                                           version("0.1"),
+                                                                           port(port),
+                                                                           password(password)
 {
     std::cout << "Server created" << std::endl;
     std::cout << "Host: " << host << std::endl;
-    std::cout << "Port: " << port << std::endl; 
+    std::cout << "Port: " << port << std::endl;
     std::cout << "Password: " << password << std::endl;
     std::cout << "Servername: " << servername << std::endl;
-    
+
     this->create_socket();
     this->createPoll();
 }
 
 void ft::Server::create_socket()
 {
-    struct sockaddr_in addr; // option to add to master_fd 
-    int opt = 1;
-    /**
-     * Creating socket file descriptor
-     * AF_INET: IPv4
-     * SOCK_STREAM: TCP
-     * 0: Default protocol
-     */ 
-    if ((master_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    int yes = 1;
+    addrinfo hints, *servinfo;
+    std::memset(&hints, 0, sizeof(addrinfo));
+
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_CANONNAME;
+
+    if (getaddrinfo(host.c_str(), this->port.c_str(), &hints, &servinfo) != 0)
     {
-        std::cout << "Socket failed" << std::endl;
-        exit(EXIT_FAILURE);
+        std::cerr << "getaddrinfo" << std::endl;
+        exit(1);
     }
-    /**
-     * Forcefully attaching socket to the port 8080
-     * SO_REUSEADDR: Reuse the address
-     * SO_REUSEPORT: Reuse the port
-     * SOL_SOCKET: Socket layer itself
-     * &opt: Pointer to the option value
-     * sizeof(opt): Size of the option value
-     */
-    if (setsockopt(master_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)))
+
+    if ((this->master_fd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol)) == -1)
     {
-        std::cout << "Setsockopt failed" << std::endl;
-        exit(EXIT_FAILURE);
+        std::cerr << "socket" << std::endl;
+        exit(1);
     }
-    addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = INADDR_ANY;
-    addr.sin_port = htons(atoi(port.c_str()));
-    /**
-     * Forcefully attaching socket to the port 8080
-     * fd: Socket file descriptor
-     * (struct sockaddr *)&addr: Pointer to the address
-     * sizeof(addr): Size of the address
-     */
-    if (bind(master_fd, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+
+    if (setsockopt(this->master_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
     {
-        std::cout << "Bind failed" << std::endl;
-        exit(EXIT_FAILURE);
+        std::cerr << "setsockopt" << std::endl;
+        exit(1);
     }
-    if (listen(master_fd, 3) < 0)
+
+    if (bind(this->master_fd, servinfo->ai_addr, servinfo->ai_addrlen) == -1)
     {
-        std::cout << "Listen failed" << std::endl;
-        exit(EXIT_FAILURE);
+        std::cerr << "bind" << std::endl;
+        exit(1);
     }
-    std::cout << "Server running" << std::endl;
+
+    if (listen(this->master_fd, 10) == -1)
+    {
+        std::cerr << "listen" << std::endl;
+        exit(1);
+    }
+    std::cout << "Server listening on " << servinfo->ai_canonname << ":" << port << std::endl;
 }
 
 void ft::Server::createPoll()
 {
     pollfd pfd;
-    pfd.fd = master_fd;
+    pfd.fd = this->master_fd;
     pfd.events = POLLIN;
-    fds.push_back(pfd);
+    this->fds.push_back(pfd);
+
+    std::cout << "Server is online" << std::endl;
 
     while (1)
     {
-        int ret = poll(fds.data(), fds.size(), -1);
+        int ret = poll(&this->fds[0], this->fds.size(), -1);
         if (ret == -1)
         {
-            if (errno == EINTR)
-                continue;
-            std::cout << "Poll failed" << std::endl;
-            exit(EXIT_FAILURE);
+            std::cerr << "poll" << std::endl;
+            exit(1);
         }
-        for (size_t i = 0; i < fds.size(); i++)
+        else if (ret > 0)
         {
-            if (fds[i].revents & POLLIN)
+            for (size_t i = 0; i < this->fds.size(); i++)
             {
-                if (fds[i].fd == master_fd)
+                if (this->fds[i].revents & POLLIN)
                 {
-                    struct sockaddr_in addr;
-                    socklen_t addrlen = sizeof(addr);
-                    int client_fd = accept(master_fd, (struct sockaddr *)&addr, &addrlen);
-                    if (client_fd == -1)
+                    if (this->fds[i].fd == this->master_fd)
                     {
-                        std::cout << "Accept failed" << std::endl;
-                        exit(EXIT_FAILURE);
+                        this->acceptConnection();
                     }
-                    std::cout << "New connection" << std::endl;
-                    Client client(client_fd);
-                    clients.push_back(&client);
-                    struct pollfd pfd;
-                    pfd.fd = client_fd;
-                    pfd.events = POLLIN;
-                    fds.push_back(pfd);
-                }
-                else if (i > 0)
-                {
-                    //print client number and message
-                    std::cout << "Client " << clients[i - 1]->fd << " sent a message" << std::endl;
-                    char buffer[1024] = {0}; // 1024 is the max size of the message
-                    int valread = read(fds[i].fd, buffer, 1024); // read from client
-                    if (valread == 0)
+                    else
                     {
-                        std::cout << "Client disconnected" << std::endl;
-                        close(fds[i].fd);
-                        fds.erase(fds.begin() + i);
-                        clients.erase(clients.begin() + i - 1);
-                        continue;
+                        this->receiveMessage(i);
                     }
-                    std::cout << buffer << std::endl;
-                    //send message to all clients -- > test only 
-                    for (size_t j = 1; j < fds.size(); j++)
-                    {
-                        if (fds[j].fd != master_fd)
-                        {
-                            send(fds[j].fd, buffer, strlen(buffer), 0);
-                        }
-                    }
-                    /**
-                     * this section will use to handle the message
-                    */
-                    Message msg(buffer);
                 }
             }
         }
     }
 }
 
+void *get_in_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET)
+        return &(((struct sockaddr_in *)sa)->sin_addr);
+    return &(((struct sockaddr_in6 *)sa)->sin6_addr);
+}
+
+void ft::Server::acceptConnection()
+{
+    int new_fd;
+    sockaddr_storage their_addr;
+    their_addr.ss_family = AF_INET;
+    their_addr.__ss_align = 0;
+    socklen_t addr_size = sizeof(their_addr);
+    char s[INET6_ADDRSTRLEN];
+
+    if ((new_fd = accept(master_fd, (sockaddr *)&their_addr, &addr_size)) == -1)
+    {
+        std::cerr << "accept" << std::endl;
+        exit(1);
+    }
+
+    inet_ntop(their_addr.ss_family, get_in_addr((sockaddr *)&their_addr), s, sizeof(s));
+    std::cout << "New connection from " << s << std::endl;
+
+    Client *client = new Client(new_fd, servername);
+    this->clients.push_back(client);
+    
+    pollfd pfd;
+    pfd.fd = new_fd;
+    pfd.events = POLLIN;
+    pfd.revents = 0;
+    fds.push_back(pfd);
+}
+
+void ft::Server::receiveMessage(int i)
+{
+    int nbytes;
+    char buf[1024];
+    if ((nbytes = recv(fds[i].fd, buf, 1024, 0)) <= 0)
+    {
+        if (nbytes == 0)
+        {
+            std::cout << "socket " << fds[i].fd << " hung up" << std::endl;
+        }
+        else
+        {
+            std::cerr << "recv" << std::endl;
+        }
+        close(fds[i].fd);
+        fds.erase(fds.begin() + i);
+    }
+    else
+    {
+        buf[nbytes] = '\0';
+        std::cout << "Received: " << buf <<"----"<< std::endl;
+        std::cout << clients[i- 1]->fd << std::endl;
+    }
+}
 
 ft::Server::~Server()
 {
