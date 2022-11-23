d06dccdc935ca7f944be9c8a87758b59f8967fab
Saxsori <jungxbra@gmail.com>
2022-11-23 05:28:12 +0400

    channel name parsing

--- src/Channel.cpp
+++ src/Channel.cpp
@@ -1,13 +1,13 @@
 #include "../incl/Channel.hpp"
 
-ft::Channel::Channel(std::string &name,  std::string &password): _name(name), _password(password), _max_clients(255), _creator(NULL) {
-	_created_at = time(0);
-}
-
-ft::Channel::Channel(/* args */)
+ft::Channel::Channel(std::string &name,  std::string &password): _password(password), _max_clients(255), _creator(NULL) 
 {
+	if (!this->_ChName_parse(std::string &name)
+		throw Channel::WrongChannelNameRequir();
+	_created_at = time(0);
 }
 
 ft::Channel::~Channel()
 {
 }
+
