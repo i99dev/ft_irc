3bc68094f35548425f4e3fb4c5bf9b43b44c31ca
Imad Saad <em4d2002@gmail.com>
2022-11-29 15:53:38 +0400

    join fixing...

--- src/cmd/Join.cpp
+++ src/cmd/Join.cpp
@@ -9,8 +9,68 @@ ft::Join::Join(void)
 
 void ft::Join::execute()
 {
-    // get channel ;ist from server
-    std::vector<Channel *> channels = _server->getChannels();
+    	// get channel ;ist from server
+	std::vector<Channel *> channels = _server->getChannels();
+	int Count = 1;
+
+
+
+	char *s;
+	std::string cmd = _message->getParameter()[0];
+	std::vector<std::string> chName; // to store all channel names
+	std::vector<std::string> passwords; // to store all passwords
+
+	int j = 0;
+	int size = 0;
+	int start = 0;
+	// count the number of channels written in one command
+	for(int i = 0; i < int(cmd.size()); i++){
+		if (cmd[i] == ',')
+			Count++;
+	}
+	std::cout << "count: " << Count << std::endl;
+	// loop to get all the channel names one by one
+	for(int i = 0; i < Count; i++){
+		size = 0;
+		if (cmd[j] != ',' && cmd[j])
+			start = j;
+		while(j < int(cmd.size())){
+			if (cmd[j] == ',')
+				break ;
+			j++;
+			size++;
+		}
+		s = new char[size + 1];
+		cmd.copy(s, size - 1, start + 1);
+		s[size] = 0;
+		chName.push_back(std::string(s));
+		delete [] s;
+		j++;
+	}
+	j = 0;
+	start = 0;
+	size = 0;
+	cmd = _message->getParameter()[1];
+	// loop to get all the passwords one by one
+	for(int i = 0; i < Count; i++){
+		size = 0;
+		if (cmd[j] != ',' && cmd[j])
+			start = j;
+		while(j < int(cmd.size())){
+			if (cmd[j] == ',')
+				break ;
+			j++;
+			size++;
+		}
+		s = new char[size + 1];
+		cmd.copy(s, size, start);
+		s[size] = 0;
+		passwords.push_back(std::string(s));
+		delete [] s;
+		j++;
+	}
+
+
     // get channel name from message
     std::string channelName = _message->getParameter()[0];
     // get channel key from message
