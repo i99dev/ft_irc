d30c144186a5821132e125575d02109e52af7a9d
Imad Saad <em4d2002@gmail.com>
2022-11-29 16:10:23 +0400

    join working on fixing it...

--- src/cmd/Join.cpp
+++ src/cmd/Join.cpp
@@ -9,6 +9,7 @@ ft::Join::Join(void)
 
 void ft::Join::execute()
 {
+	int flag = 0;
     	// get channel ;ist from server
 	std::vector<Channel *> channels = _server->getChannels();
 	int Count = 1;
@@ -71,55 +72,71 @@ void ft::Join::execute()
 	}
 
 
-    // get channel name from message
-    std::string channelName = _message->getParameter()[0];
-    // get channel key from message
-    std::string channelKey = "";
-    // if ( _message->getParameter()[1] != "")
-    // {
-    //     std::string channelKey = _message->getParameter()[1];
-    // }
-    // check if channel exists
-    std::vector<Channel *>::iterator it;
-    for (it = channels.begin(); it != channels.end(); it++)
-    {
-        if ((*it)->getChName() == channelName)
-        {
-            // check if channel has a key
-            if ((*it)->getpassword() != "")
-            {
-                // check if key is correct
-                if ((*it)->getpassword() == channelKey)
-                {
-                    // add client to channel
-                    (*it)->addUser(_client);
-                    // send message to client
-                    std::string joinMsg = ":" + _client->getNickName() + " JOIN :" + (*it)->getChName();
-                    _client->sendReply(joinMsg);
-                }
-                else
-                {
-                    // send message to client
-                    std::string errMsg = ERR_BADCHANNELKEY(_server->getServerName(), _client->getNickName(), channelName);
-                    _client->sendReply(errMsg);
-                }
-            }
-            else
-            {
-                // add client to channel
-                (*it)->addUser(_client);
-                // send message to client
-                std::string joinMsg = ":" + _client->getNickName() + " JOIN :" + (*it)->getChName();
-                _client->sendReply(joinMsg);
-            }
-        }
-    }
-    //if channel does not exist create 
-    Channel *channel = new Channel(_client,channelName);
-    // channel->addUser(_client); // don't uncomment without Ibraar authroztion !!! :) 
-    _server->channels.push_back(channel);
-    // send message to client
-    std::string joinMsg = ":" + _client->getNickName() + " JOIN :" + channel->getChName();
-    _client->sendReply(joinMsg);
+	for (int i = 0; i < Count; i++){
+		flag = 0;
+	    // get channel name from message
+	    std::string channelName = "&" + chName[i];
+		std::cout << "channel name: " << channelName << std::endl;
+	    // get channel key from message
+	    std::string channelKey = "";
+	    if (!passwords[i].empty())
+	    {
+	        std::string channelKey = passwords[i];
+			std::cout << "channel key: " << channelKey << std::endl;
+	    }
+	    // check if channel exists
+	    std::vector<Channel *>::iterator it;
+	    for (it = channels.begin(); it != channels.end(); it++)
+	    {
+	        if ((*it)->getChName() == channelName)
+	        {
+	            // check if channel has a key
+	            if ((*it)->getpassword() != "")
+	            {
+	                // check if key is correct
+	                if ((*it)->getpassword() == channelKey)
+	                {
+	                    // add client to channel
+	                    (*it)->addUser(_client);
+	                    // send message to client
+	                    std::string joinMsg = ":" + _client->getNickName() + " JOIN :" + (*it)->getChName();
+	                    _client->sendReply(joinMsg);
+						flag = 1;
+						break;
+						// return ;
+	                }
+	                else
+	                {
+	                    // send message to client
+	                    std::string errMsg = ERR_BADCHANNELKEY(_server->getServerName(), _client->getNickName(), channelName);
+	                    _client->sendReply(errMsg);
+						flag = 1;
+						break;
+						// return ;
+	                }
+	            }
+	            else
+	            {
+	                // add client to channel
+	                (*it)->addUser(_client);
+	                // send message to client
+	                std::string joinMsg = ":" + _client->getNickName() + " JOIN :" + (*it)->getChName();
+	                _client->sendReply(joinMsg);
+					flag = 1;
+					break;
+					// return ;
+	            }
+	        }
+	    }
+		if (flag == 0 && i == Count - 1){
+		    //if channel does not exist create 
+		    Channel *channel = new Channel(_client,channelName);
+		    // channel->addUser(_client); // don't uncomment without Ibraar authroztion !!! :) 
+		    _server->channels.push_back(channel);
+		    // send message to client
+		    std::string joinMsg = ":" + _client->getNickName() + " JOIN :" + channel->getChName();
+		    _client->sendReply(joinMsg);
+		}	
+	}
 }
     
