b6d4b4b70beaa0ccd17ad0233a9a4c0d8652f459
i99dev <obaid.altuniji@gmail.com>
2022-11-30 07:09:41 +0400

    error respos fix it

---
+++ src/cmd/Ping.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Ping.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oal-tena <oal-tena@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/30 07:06:48 by oal-tena          #+#    #+#             */
+/*   Updated: 2022/11/30 07:07:22 by oal-tena         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../incl/cmd/Ping.hpp"
+
+ft::Ping::Ping()
+{
+    _name = "Ping";
+    _description = "Ping the server";
+    _usage = "/ping";
+}
+
+void ft::Ping::execute()
+{
+    std::cout << "Ping executed" << std::endl;
+}
