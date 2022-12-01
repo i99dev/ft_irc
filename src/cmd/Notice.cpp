e6fb2bac1916676fef7b19e006677ddf900d9d75
Imad Saad <em4d2002@gmail.com>
2022-12-01 15:02:18 +0400

    join fixed & notice fixed

--- src/cmd/Notice.cpp
+++ src/cmd/Notice.cpp
@@ -6,7 +6,7 @@
 /*   By: isaad <isaad@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 00:14:34 by oal-tena          #+#    #+#             */
-/*   Updated: 2022/12/01 05:05:25 by isaad            ###   ########.fr       */
+/*   Updated: 2022/12/01 14:53:19 by isaad            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ void ft::Notice::execute(){
 			std::vector<Client *>::iterator it2 = clients.begin();
 			for (; it2 != clients.end(); it2++)
 			{
-				joinMsg = ":" + _client->getNickName() + " NOTICE " + msg;
+				joinMsg = ":" + _client->getNickName() + " NOTICE " + target + " :" + msg;
 				(*it2)->sendReply(joinMsg);
 			}
 		}
@@ -39,7 +39,7 @@ void ft::Notice::execute(){
 	for (int i = 0; i < int(_server->clients.size()); i++){
 		if (_server->clients[i]->getNickName() == target){
 			flag = 1;
-			joinMsg = ":" + _client->getNickName() + " NOTICE " + msg;
+			joinMsg = ":" + _client->getNickName() + " NOTICE " + target + " :" + msg;;
 			_server->clients[i]->sendReply(joinMsg);
 		}
 	}
