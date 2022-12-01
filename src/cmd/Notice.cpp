de2778f705d32f7b8a1decd65fa97af88566a3c0
Imad Saad <em4d2002@gmail.com>
2022-12-01 05:06:33 +0400

    notice error added

--- src/cmd/Notice.cpp
+++ src/cmd/Notice.cpp
@@ -6,7 +6,7 @@
 /*   By: isaad <isaad@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 00:14:34 by oal-tena          #+#    #+#             */
-/*   Updated: 2022/12/01 04:54:53 by isaad            ###   ########.fr       */
+/*   Updated: 2022/12/01 05:05:25 by isaad            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,11 @@ void ft::Notice::execute(){
 	std::string target = _message->getParameter()[0];
 	std::string msg = _message->getParameter()[1];
 	std::string joinMsg = "";
+	int flag = 0;
 
 	for (int i = 0; i < int(_server->channels.size()); i++){
 		if (_server->channels[i]->getChName() == target){
+			flag = 1;
 			std::vector<Client *> clients = (_server->getChannels()[i])->getUsers();
 			std::vector<Client *>::iterator it2 = clients.begin();
 			for (; it2 != clients.end(); it2++)
@@ -36,8 +38,12 @@ void ft::Notice::execute(){
 	}
 	for (int i = 0; i < int(_server->clients.size()); i++){
 		if (_server->clients[i]->getNickName() == target){
+			flag = 1;
 			joinMsg = ":" + _client->getNickName() + " NOTICE " + msg;
 			_server->clients[i]->sendReply(joinMsg);
 		}
+	}
+	if (flag == 0){
+		_client->sendReply(ERR_NOSUCHNICK(_server->getServerName(), target));
 	}
 }
