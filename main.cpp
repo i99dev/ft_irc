c31966dabf63ae14f4de02b7ddc4e1798f1fe77a
i99dev <obaid.altuniji@gmail.com>
2022-12-11 00:34:46 +0400

    add some stuff to main

--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aaljaber <aaljaber@student.42abudhabi.a    +#+  +:+       +#+        */
+/*   By: oal-tena <oal-tena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 10:55:16 by oal-tena          #+#    #+#             */
-/*   Updated: 2022/12/01 20:46:57 by aaljaber         ###   ########.fr       */
+/*   Updated: 2022/12/11 00:34:32 by oal-tena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,19 @@ int main(int argc, char **argv)
     if (argc != 3)
     {
         std::cout << "Usage: ./ircserv [port] [password]" << std::endl;
+        return (EXIT_FAILURE);
+    }
+    //check password
+    if (strlen(argv[2]) < 6)
+    {
+        std::cout << "Password must be at least 6 characters" << std::endl;
+        return (EXIT_FAILURE);
+    }
+    //check port
+    int port = atoi(argv[1]);
+    if (port < 1024 || port > 65535)
+    {
+        std::cout << "Port must be between 1024 and 65535" << std::endl;
         return (EXIT_FAILURE);
     }
     ft::Server server(argv[1], argv[2]);
