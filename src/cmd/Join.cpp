e4f61639ee2668f23798a3049ce9f71e33848a66
Imad Saad <em4d2002@gmail.com>
2022-12-12 11:36:12 +0400

    join fixed

--- src/cmd/Join.cpp
+++ src/cmd/Join.cpp
@@ -2,15 +2,15 @@
 
 ft::Join::Join(void)
 {
-    _name = "Join";
-    _description = "Join a channel";
-    _usage = "/join <channel> [key]";
+	_name = "Join";
+	_description = "Join a channel";
+	_usage = "/join <channel> [key]";
 }
 
 void ft::Join::execute()
 {
 	int flag = 0;
-    	// get channel ;ist from server
+		// get channel ;ist from server
 	std::vector<Channel *> channels = _server->getChannels();
 
 	std::string cmd = _message->getParameter()[0];
@@ -37,40 +37,42 @@ void ft::Join::execute()
 
 	i = 0;
 	s.clear();
-	while(_message->getParameter()[1][i] != '\0')
-	{
-		if(_message->getParameter()[1][i] != ',')
+	if (_message->getParameter().size() > 1){
+		while(_message->getParameter()[1][i] != '\0')
 		{
-			s += _message->getParameter()[1][i];   // append the char array to the temp string
+			if(_message->getParameter()[1][i] != ',')
+			{
+				s += _message->getParameter()[1][i];   // append the char array to the temp string
+			}
+			else{
+				passwords.push_back(s);
+				s.clear();
+			}
+			i++;
+	 
 		}
-		else{
-			passwords.push_back(s);
-			s.clear();
-		}
-		i++;
- 
+		passwords.push_back(s);
 	}
-	passwords.push_back(s);
 
 	for (int i = 0; i < int(chName.size()); i++){
 		flag = 0;
 		bool ok = false;
-	    // get channel name from message
-	    std::string channelName = chName[i];
-	    // get channel key from message
-	    std::string channelKey = "";
-	    if (_message->getParameter().size() > 1)
-	    {
+		// get channel name from message
+		std::string channelName = chName[i];
+		// get channel key from message
+		std::string channelKey = "";
+		if (_message->getParameter().size() > 1)
+		{
 			if (!passwords[i].empty()){
-		        channelKey = passwords[i];
+				channelKey = passwords[i];
 			}
-	    }
-	    // check if channel exists
-	    // std::vector<Channel *>::iterator it;
-	    for (int i = 0; i < int(channels.size()); i++)
-	    {
-	        if (channels[i]->getChName() == channelName)
-	        {
+		}
+		// check if channel exists
+		// std::vector<Channel *>::iterator it;
+		for (int i = 0; i < int(channels.size()); i++)
+		{
+			if (channels[i]->getChName() == channelName)
+			{
 				flag = 1;
 				if (channels[i]->isCHModeSet('i')){
 					for (int y = 0; y < int(_client->invites.size()); y++){
@@ -83,37 +85,37 @@ void ft::Join::execute()
 						return ;
 					}
 				}
-	            // check if channel has a key
-	            if (channels[i]->getpassword() != "")
-	            {
-	                // check if key is correct
-	                if (_message->getParameter()[1].size() > 0 && channels[i]->getpassword() == channelKey)
-	                {
-	                    // add client to channel
-	                    channels[i]->addUser(_client);
-	                    // send message to all clients in that channel
+				// check if channel has a key
+				if (channels[i]->getpassword() != "")
+				{
+					// check if key is correct
+					if (_message->getParameter()[1].size() > 0 && channels[i]->getpassword() == channelKey)
+					{
+						// add client to channel
+						channels[i]->addUser(_client);
+						// send message to all clients in that channel
 						std::vector<Client *> clients = (channels[i])->getUsers();
 						std::vector<Client *>::iterator it2 = clients.begin();
 						for (; it2 != clients.end(); it2++)
 						{
 							// std::string joinMsg = ":" + _client->getNickName() + " NOTICE " + target + " :" + msg;
-	                    	std::string joinMsg = ":" + _client->getNickName() + " JOIN " + channels[i]->getChName();
+							std::string joinMsg = ":" + _client->getNickName() + " JOIN " + channels[i]->getChName();
 							(*it2)->sendReply(joinMsg);
 						}
-	                    // _client->sendReply(joinMsg);
-	                }
-	                else
-	                {
-	                    // send message to client
-	                    std::string errMsg = ERR_BADCHANNELKEY(_server->getServerName(), _client->getNickName(), channelName);
-	                    _client->sendReply(errMsg);
-	                }
-	            }
-	            else
-	            {
-	                // add client to channel
-	                channels[i]->addUser(_client);
-	                // send message to all clients in that channel
+						// _client->sendReply(joinMsg);
+					}
+					else
+					{
+						// send message to client
+						std::string errMsg = ERR_BADCHANNELKEY(_server->getServerName(), _client->getNickName(), channelName);
+						_client->sendReply(errMsg);
+					}
+				}
+				else
+				{
+					// add client to channel
+					channels[i]->addUser(_client);
+					// send message to all clients in that channel
 					std::vector<Client *> clients = (channels[i])->getUsers();
 					std::vector<Client *>::iterator it2 = clients.begin();
 					for (; it2 != clients.end(); it2++)
@@ -129,23 +131,23 @@ void ft::Join::execute()
 					else
 						topicMsg = ":" + _client->getServerName() + " 331 " + _client->getNickName() + " " + _client->getNickName() + " " + channelName + " :No topic is set";
 					_client->sendReply(topicMsg);
-	            }
-	        }
-	    }
+				}
+			}
+		}
 		if (flag == 0){
-		    //if channel does not exist create 
-		    Channel *channel = new Channel(_client,channelName);
+			//if channel does not exist create 
+			Channel *channel = new Channel(_client,channelName);
 			if (channelKey != "" || channelKey != "x"){
 				channel->setPassword(channelKey);
 			}
-		    // channel->addUser(_client); // don't uncomment without Ibraar authroztion !!! :) 
-		    _server->channels.push_back(channel);
-		    // send message to client
-		    std::string joinMsg = ":" + _client->getNickName() + " JOIN " + channel->getChName();
-		    _client->sendReply(joinMsg);
+			// channel->addUser(_client); // don't uncomment without Ibraar authroztion !!! :) 
+			_server->channels.push_back(channel);
+			// send message to client
+			std::string joinMsg = ":" + _client->getNickName() + " JOIN " + channel->getChName();
+			_client->sendReply(joinMsg);
 			std::cout << "3 channel name " << channel->getChName() << std::endl;
 			std::string topicMsg = RPL_TOPIC(_server->getServerName(), _client->getNickName(), channel->getChName(), channel->getTopic());
-		    _client->sendReply(topicMsg);
+			_client->sendReply(topicMsg);
 		}
 	}
 }
