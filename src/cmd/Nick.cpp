da26235f179bb1683d7dbeae0576ddc416f61c12
Imad Saad <em4d2002@gmail.com>
2022-12-27 19:35:41 +0400

    nick fixes

--- src/cmd/Nick.cpp
+++ src/cmd/Nick.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Nick.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aaljaber <aaljaber@student.42abudhabi.ae>  +#+  +:+       +#+        */
+/*   By: isaad <isaad@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 00:14:34 by oal-tena          #+#    #+#             */
-/*   Updated: 2022/12/27 14:45:08 by aaljaber         ###   ########.fr       */
+/*   Updated: 2022/12/27 19:30:45 by isaad            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,8 +34,11 @@ void	ft::Nick::connectClientBack()
 			{
 				if (_server->channels[i]->members[j].user->fd == _server->CLIENTBACKFD && _server->channels[i]->members[j].user_mode != O_CHANNEL_CREATOR)
 				{
-					std::string joinMsg = ":" + _message->getParameter()[0] + " JOIN " + _server->channels[i]->getChName();
-					_server->channels[i]->members[j].user->sendReply(joinMsg);					
+					std::string joinMsg = ":" + _message->getParameter()[0] + " PART " + _server->channels[i]->getChName() + " :" + "bye";
+					_server->channels[i]->members[j].user->sendReply(joinMsg);
+					ft::Client *tmp = _server->channels[i]->members[j].user;
+					joinMsg = ":" + _message->getParameter()[0] + " JOIN " + _server->channels[i]->getChName();
+					tmp->sendReply(joinMsg);					
 				}
 			}
 		}
