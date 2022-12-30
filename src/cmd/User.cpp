6731aa8bde6a4b8f27584b59fde832247ab43895
Imad Saad <em4d2002@gmail.com>
2022-12-31 00:08:24 +0400

    gg

--- src/cmd/User.cpp
+++ src/cmd/User.cpp
@@ -6,7 +6,7 @@
 /*   By: isaad <isaad@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 19:18:38 by oal-tena          #+#    #+#             */
-/*   Updated: 2022/12/30 23:09:49 by isaad            ###   ########.fr       */
+/*   Updated: 2022/12/31 00:08:15 by isaad            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,12 +34,6 @@ void ft::User::execute()
 			_client->setRealName(_message->getParameter()[3]);
 			std::cout << "User executed" << std::endl;
 			for (long unsigned int i = 0; i < this->_message->getParameter().size(); i++)
-				std::cout << i << " " << this->_message->getParameter()[i] << std::endl; 
-			// std::string msg = \
-			// RPL_WELCOME(_message->getParameter()[2], _message->getParameter()[0]) + \
-			// RPL_YOURHOST("42_irc", _message->getParameter()[0], "0.1") + \
-			// RPL_CREATED("42_irc", _message->getParameter()[0]) + \
-			// RPL_MYINFO("42_irc", _message->getParameter()[0], "0.1", "iow", "ov");
-			// _server->sendReply(_client, msg);
+				std::cout << i << " " << this->_message->getParameter()[i] << std::endl;
 	}
 }
